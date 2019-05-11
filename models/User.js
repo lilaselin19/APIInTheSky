47941ea64855cf87e2ce4438a25d421fde4d205a
varunkhanna19 <31697566+varunkhanna19@users.noreply.github.com>
2019-05-11 12:39:33 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ var GoogleSpreadsheet = require('google-spreadsheet');
 var creds = require(__dirname+'/client_secret.json');
 var doc = new GoogleSpreadsheet('1x7OjTW1T54u8R5MM7OZUCTsLJ5r1fVi0b-gd9jt0nxc');
 
-//gets a user
+
+
 exports.getUser = function(user_id, callback) {
   var user = createBlankUser();
   var all_users = dataJS.loadGoogle(3, function(all_users) {
@@ -20,11 +21,13 @@ exports.getUser = function(user_id, callback) {
   return true;
 }
 
-//creates a user
+exports.deleteUser = function(user_id, callback) {
+  dataJS.deleteRow(user_id, callback)
+}
+
 exports.createUser = function(name, password, callback) {
     var result = true;
     var fb = 0;
-    console.log(name+" "+password)
     if (name==null||name==""||password==null||password==""){
         result= false;
         fb = 42;
@@ -41,8 +44,7 @@ exports.createUser = function(name, password, callback) {
           "Password": password.trim(),
 
         }
-        console.log(new_obj)
-        dataJS.createRow(new_obj, 3, function(){
+        dataJS.createRow(new_obj, 1, function(){
           callback(true, fb);
         })
       } else {
@@ -51,15 +53,8 @@ exports.createUser = function(name, password, callback) {
   })
 }
 
-
-//deletes a user
-exports.deleteUser = function(user_id, callback) {
-  dataJS.deleteRow(user_id, callback)
-}
-
-//updates the date for a user
 exports.updateUser = function(user_id, updates, callback) {
-  dataJS.updateRow(2, user_id, updates, function(){
+  dataJS.updateRow(1, user_id, updates, function(){
     console.log("doing next");
     callback();
   });
@@ -70,7 +65,6 @@ var createBlankUser= function(){
     name:"notarealuser",
     password:"test",
     email: "email"
-    
   };
   return user;
 }
